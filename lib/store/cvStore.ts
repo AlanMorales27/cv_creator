3a2264c55efce8eb001fcf66290b20453a1b9fd2
AlanMorales27 <alanmoralesg47@gmail.com>
2026-05-13 20:25:22 -0500

    feat: put photo in the cv

--- lib/store/cvStore.ts
+++ lib/store/cvStore.ts
@@ -1,13 +1,13 @@
 import { create } from 'zustand'
 import { CvShape, PersonalItem, SectionItem } from '@/lib/schemas'
-import { CvData } from '../cv_mock_data'
+import { CvData } from '../data/cv_mock_data'
 import { persist } from 'zustand/middleware'
 
 type CvStoreActions = {
-    addSection:         (section: SectionItem) => void,
+    addSection: (section: SectionItem) => void,
     updatePersonalInfo: (info: PersonalItem) => void,
-    updateSummary:      (summary: string) => void,
-    updateSection:      (id: number, data: SectionItem) => void,
+    updateSummary: (summary: string) => void,
+    updateSection: (id: number, data: SectionItem) => void,
 }
 
 type CvStoreState = CvShape & CvStoreActions
@@ -15,16 +15,18 @@ type CvStoreState = CvShape & CvStoreActions
 const useCvStore = create<CvStoreState>()(
     persist(set => ({
         personalInfo: CvData.personalInfo,
-        summary:      CvData.summary,
-        sections:     CvData.sections,
+        summary: CvData.summary,
+        sections: CvData.sections,
 
         addSection: (section: SectionItem) => set(state => ({
             sections: [...state.sections, section]
         })),
+
         updatePersonalInfo: (info: PersonalItem) => set({ personalInfo: info }),
-        updateSummary:      (summary: string) => set({ summary }),
         
-        updateSection:      (id: number, data: SectionItem) => set(state => ({
+        updateSummary: (summary: string) => set({ summary }),
+
+        updateSection: (id: number, data: SectionItem) => set(state => ({
             sections: state.sections.map(s => s.id === id ? data : s)
         })),
     }),
